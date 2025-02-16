c2656eb2dfb1b68134774d60a8e020caa67805fd
Xeuz27 <jesusg.1301.jg@gmail.com>
2025-02-15 22:48:55 -0400

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dogor.vercel.app/',
+    site: 'https://dogor-linktree.vercel.app/',
     integrations: [tailwind()],
     output: 'server',
     adapter: vercel(),
